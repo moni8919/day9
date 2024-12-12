a02af106423ba55d47e19bfec4a968a60b122528
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-12 15:17:33 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class main {
+    public static void main(String[] args){
+        ArrayList<Integer>a=new ArrayList<>();
+        a.add(90);
+        a.add(89);
+        a.add(767);
+        a.remove(2);
+        System.out.println(a);
+        a.clear();
+        System.out.println(a);
+    }
+    
+}
